782bb5374bef8cc4fb21b7c8900b1a2fceb50d9a
rukhsar624 <rukhsaranwar56@gmail.com>
2022-12-14 14:37:03 +0500

    add avg formula changing

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,16 +32,14 @@ Slider1.addEventListener('input', () => {
     document.getElementById('range1').innerHTML = Slider1.value + '%'
     console.log('range1', Slider1.value)
     weight[0] = Number(Slider1.value) / 100
-    console.log(weight, "hello1")
     getAvg1(RGB)
 
   } else {
     document.getElementById('range1').innerHTML = Slider1.value + '%'
     console.log('range1', Slider1.value)
     console.log(Number(Slider1.value) / 100)
-    getAvg1(RGB)
     weight[0] = Number(Slider1.value) / 100;
-    console.log(weight, "hello1");
+    getAvg1(RGB)
   }
 })
 Slider2.addEventListener('input', () => {
@@ -49,15 +47,13 @@ Slider2.addEventListener('input', () => {
     document.getElementById('range2').innerHTML = Slider2.value + '%'
     console.log('range2', Slider2.value)
     weight[1] = Number(Slider1.value) / 100
-    console.log(weight, "hello2")
     getAvg1(RGB)
   } else {
     document.getElementById('range2').innerHTML = Slider2.value + '%'
     console.log('range2', Slider2.value)
     console.log(Number(Slider2.value) / 100)
-    getAvg1(RGB)
     weight[1] = Number(Slider2.value) / 100;
-    console.log(weight, "hello2");
+    getAvg1(RGB)
   }
 })
 Slider3.addEventListener('input', () => {
@@ -65,15 +61,13 @@ Slider3.addEventListener('input', () => {
     document.getElementById('range3').innerHTML = Slider3.value + '%'
     console.log('range3', Slider3.value)
     weight[2] = Number(Slider3.value) / 100
-    console.log(weight, "hello3")
     getAvg1(RGB)
   } else {
     document.getElementById('range3').innerHTML = Slider3.value + '%'
     console.log('range3', Slider3.value)
     console.log(Number(Slider3.value) / 100)
-    getAvg1(RGB)
     weight[2] = Number(Slider3.value) / 100;
-    console.log(weight, "hello3");
+    getAvg1(RGB)
   }
 })
 
@@ -81,13 +75,20 @@ Slider3.addEventListener('input', () => {
 var getAvg1 = function (list) {
   var rgb_total = { r: 0, g: 0, b: 0 }
   var weight_total = 0;
-  console.log(list, 'color aray')
   list.map((elem, i) => {
-    rgb_total.r = (rgb_total.r + Number(elem.r)) * weight[i]
-    rgb_total.g = (rgb_total.g + Number(elem.g)) * weight[i]
-    rgb_total.b = (rgb_total.b + Number(elem.b)) * weight[i]
+    rgb_total.r = rgb_total.r + Number(elem.r) * weight[i]
+    rgb_total.g = rgb_total.g + Number(elem.g) * weight[i]
+    rgb_total.b = rgb_total.b + Number(elem.b) * weight[i]
     weight_total = weight_total + weight[i]
-    console.log(rgb_total, weight_total)
+    console.log(weight[i], 'color weight')
+    console.log(weight_total, 'weight_total')
+    console.log(elem, 'color')
+    console.log(rgb_total, 'rgb_total')
+
+    //  console.log(l.weight, 'color weight');
+    //     console.log(weight_total, 'weight_total');
+    //     console.log(hexToR(l.color),hexToG(l.color),hexToB(l.color), 'color');
+    //     console.log(rgb_total, 'rgb_total')
   })
 
   var rgbMix = {
@@ -95,13 +96,13 @@ var getAvg1 = function (list) {
     g: ~~(rgb_total.g / weight_total),
     b: ~~(rgb_total.b / weight_total),
   }
-  console.log(rgbMix, 'rgbMix')
+  // console.log(rgbMix, 'rgbMix')
   setElem(rgbMix)
 }
 
 const setElem = (rgbMix) => {
   let rgbMix1 = `rgb(${rgbMix.r}, ${rgbMix.g}, ${rgbMix.b})`
-  console.log(rgbMix1, 'Mixed Color')
+  // console.log(rgbMix1, 'Mixed Color')
   document.getElementById('rgbResult').style.backgroundColor = rgbMix1
 }
 $('button').on('click', function () {
@@ -137,28 +138,40 @@ $('button').on('click', function () {
   console.log(fullOne, colorOne, fullTwo, colorTwo, fullThree, colorThree)
   if (fullOne1 && colorOne1 == this.value) {
     document.getElementById('4').style.background = '#fff'
+    // document.getElementById('5').style.display = 'none'
+    // document.getElementById('range1').style.display = 'none'
     fullOne1 = false
     colorOne1 = ''
     firstRun1++
   } else if (fullTwo1 && colorTwo1 == this.value) {
     document.getElementById('5').style.background = '#fff'
+    // document.getElementById('5').style.display = 'none'
+    // document.getElementById('range2').style.display = 'none'
     fullTwo1 = false
     colorTwo1 = ''
     secondRun1++
   } else if (fullThree1 && colorThree1 == this.value) {
     document.getElementById('6').style.background = '#fff'
+    // document.getElementById('5').style.display = 'none'
+    // document.getElementById('range3').style.display = 'none'
     fullThree1 = false
     colorThree1 = ''
     thirdRun1++
   } else if (!fullOne1 && colorOne1 == '') {
+    // document.getElementById('4').style.display = 'block'
+    // document.getElementById('range1').style.display = 'block'
     document.getElementById('4').style.background = this.value
     colorOne1 = this.value
     fullOne1 = true
   } else if (!fullTwo1 && colorTwo1 == '') {
+    // document.getElementById('5').style.display = 'block'
+    // document.getElementById('range2').style.display = 'block'
     document.getElementById('5').style.background = this.value
     colorTwo1 = this.value
     fullTwo1 = true
   } else if (!fullThree1 && colorThree1 == '') {
+    // document.getElementById('6').style.display = 'block'
+    // document.getElementById('range3').style.display = 'block'
     document.getElementById('6').style.background = this.value
     colorThree1 = this.value
     fullThree1 = true
@@ -166,9 +179,16 @@ $('button').on('click', function () {
 
   if (!colorOne && !colorTwo && !colorThree) {
     RGB = []
+    weight=[1,1,1]
+    document.getElementById('range1').innerHTML = 100 + '%'
+    document.getElementById('range2').innerHTML = 100 + '%'
+    document.getElementById('range3').innerHTML = 100 + '%'
     document.getElementById('rgbResult').style.backgroundColor = '#fff'
   } else if (colorOne && !colorTwo && !colorThree) {
     hexArray = []
+    weight=[1,0,0]
+    document.getElementById('range2').innerHTML = 100 + '%'
+    document.getElementById('range3').innerHTML = 100 + '%'
     RGB = []
     const rgb = colorOne
       .substring(4, colorOne.length - 1)
@@ -182,10 +202,12 @@ $('button').on('click', function () {
     // const hex = rgbToHex(rgbObj)
     // console.log(hex)
     // hexArray.push(hex)
-    getAvg(RGB)
+    getAvg1(RGB)
   } else if (colorOne && colorTwo && !colorThree) {
     RGB = []
     hexArray = []
+    weight=[1,1,0]
+    document.getElementById('range3').innerHTML = 100 + '%'
     const rgb1 = colorOne
       .substring(4, colorOne.length - 1)
       .replace(/ /g, '')
@@ -204,10 +226,15 @@ $('button').on('click', function () {
     const rgbObj2 = { r: rgb2[0], g: rgb2[1], b: rgb2[2] }
     RGB.push(rgbObj2)
     console.log(RGB)
-    getAvg(RGB)
+    getAvg1(RGB)
   } else {
     RGB = []
     hexArray = []
+    weight=[1,1,1]
+    document.getElementById('range1').innerHTML = 100 + '%'
+    document.getElementById('range2').innerHTML = 100 + '%'
+    document.getElementById('range3').innerHTML = 100 + '%'
+
     const rgb1 = colorOne
       .substring(4, colorOne.length - 1)
       .replace(/ /g, '')
@@ -235,29 +262,29 @@ $('button').on('click', function () {
     const rgbObj3 = { r: rgb3[0], g: rgb3[1], b: rgb3[2] }
     RGB.push(rgbObj3)
     console.log(RGB)
-    getAvg(RGB)
+    getAvg1(RGB)
   }
   console.log('hexARray', hexArray)
 })
 
-const getAvg = (array) => {
-  let tempR, tempB, tempG
-  if (array.length == 1) {
-    tempR = array[0].r
-    tempG = array[0].g //g
-    tempB = array[0].b //g
-  } else if (array.length == 2) {
-    tempR = (Number(array[0].r) + Number(array[1].r)) / 2
-    tempG = (Number(array[0].g) + Number(array[1].g)) / 2 //g
-    tempB = (Number(array[0].b) + Number(array[1].b)) / 2 //g
-  } else {
-    tempR = (Number(array[0].r) + Number(array[1].r) + Number(array[2].r)) / 3
-    tempG = (Number(array[0].g) + Number(array[1].g) + Number(array[2].g)) / 3 //g
-    tempB = (Number(array[0].b) + Number(array[1].b) + Number(array[2].b)) / 3 //g
-  }
+// const getAvg = (array) => {
+//   let tempR, tempB, tempG
+//   if (array.length == 1) {
+//     tempR = array[0].r
+//     tempG = array[0].g //g
+//     tempB = array[0].b //g
+//   } else if (array.length == 2) {
+//     tempR = (Number(array[0].r) + Number(array[1].r)) / 2
+//     tempG = (Number(array[0].g) + Number(array[1].g)) / 2 //g
+//     tempB = (Number(array[0].b) + Number(array[1].b)) / 2 //g
+//   } else {
+//     tempR = (Number(array[0].r) + Number(array[1].r) + Number(array[2].r)) / 3
+//     tempG = (Number(array[0].g) + Number(array[1].g) + Number(array[2].g)) / 3 //g
+//     tempB = (Number(array[0].b) + Number(array[1].b) + Number(array[2].b)) / 3 //g
+//   }
 
-  let rgbMix = `rgb(${tempR}, ${tempG}, ${tempB})`
-  console.log(rgbMix, 'klkl')
-  document.getElementById('rgbResult').style.backgroundColor = rgbMix
-}
+//   let rgbMix = `rgb(${tempR}, ${tempG}, ${tempB})`
+//   console.log(rgbMix, 'klkl')
+//   document.getElementById('rgbResult').style.backgroundColor = rgbMix
+// }
 
